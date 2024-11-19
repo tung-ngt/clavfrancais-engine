dd6c3ec6ad41db3c2f2cf86aa9175d0578c85d2b
Nguyen Thanh Tung <tung.nguyent03@gmail.com>
2024-11-19 21:14:07 +0700

    housekeeping

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clavfrancais::{engine::Engine, input_controller::{setup_key_combination_map, InputController}};
+use clavfrancais::{engine::Engine, input_controller::{setup_key_combination_map}};
 
 fn main() {
     let engine = Engine::new(setup_key_combination_map());

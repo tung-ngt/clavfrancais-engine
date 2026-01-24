8a81982fa95d420f89376864cb2971d0c7ef72c0
tungngt <tung.nguyent03@gmail.com>
2026-01-24 14:57:25 +0700

    remove reverse euro

--- src/engine.rs
+++ src/engine.rs
@@ -26,9 +26,6 @@ pub fn setup_key_combination_map() -> KeyCombinationMap {
     // Replace map
     char_map.insert(KeyCombination::Single('$'), CombinationTarget::Replace('€'));
 
-    // Reverse replace map
-    char_map.insert(KeyCombination::Double('€', '€'), CombinationTarget::Combine('$'));
-
     // Combination map
     char_map.insert(KeyCombination::Double('<', '<'), CombinationTarget::Combine('«'));
     char_map.insert(KeyCombination::Double('>', '>'), CombinationTarget::Combine('»'));
